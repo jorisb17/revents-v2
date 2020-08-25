cc7b552ac3247804928ff183f37ccdfac909458e
Joris Bruinsma <joris@pop-os.localdomain>
2020-08-25 12:02:58 +0200

    added CRUD functionality

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -5,12 +5,24 @@ import { Container } from 'semantic-ui-react';
 
 function App() {
   const [formOpen, setFormOpen] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  function handleSelectEvent(event) {
+    setSelectedEvent(event);
+    setFormOpen(true);
+  }
+
+  function handleCreateFormOpen() {
+    setSelectedEvent(null);
+    setFormOpen(false);
+  }
+
 
   return (
     <div>
       <NavBar setFormOpen={setFormOpen}/>
       <Container className='main'>
-        <EventDashboard setFormOpen={setFormOpen} formOpen={formOpen}/>
+        <EventDashboard setFormOpen={handleCreateFormOpen} formOpen={formOpen} selectEvent={handleSelectEvent} selectedEvent={selectedEvent}/>
       </Container>
     </div>
   );
