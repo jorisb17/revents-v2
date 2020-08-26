1e6cfa7d0d5fbbf05ee38f0afcdd5c349e03ba92
Joris Bruinsma <joris@pop-os.localdomain>
2020-08-26 18:53:15 +0200

    added redux-thunk for async functions, added a mockApi, added a loading component, added placeholders to improve UI and added event filters

--- src/features/events/eventActions.js
+++ src/features/events/eventActions.js
@@ -1,4 +1,21 @@
-import { CREATE_EVENT, UPDATE_EVENT, DELETE_EVENT } from "./eventConstants";
+import { CREATE_EVENT, UPDATE_EVENT, DELETE_EVENT, FETCH_EVENTS } from "./eventConstants";
+import { asyncActionStart, asyncActionError, asyncActionFinish } from "../../app/async/asyncReducer";
+import { toast } from "react-toastify";
+import { fetchSampleData } from "../../app/api/mockApi";
+
+export function loadEvents() {
+    return async function(dispatch) {
+        dispatch(asyncActionStart());
+        try {
+            const events = await fetchSampleData();
+            dispatch({type: FETCH_EVENTS, payload: events});
+            dispatch(asyncActionFinish());
+        } catch (error) {
+            dispatch(asyncActionError(error));
+            toast.error(error)
+        }
+    }
+}
 
 export function createEvent(event) {
     return {
