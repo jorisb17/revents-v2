cb6ab0f34f93650ecad14501045349612f013006
Joris Bruinsma <joris@pop-os.localdomain>
2020-08-26 16:49:54 +0200

    added modalmanager, modalwrapper, auth state, auth form

--- src/features/auth/LoginForm.jsx
+++ src/features/auth/LoginForm.jsx
@@ -4,8 +4,13 @@ import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import MyTextInput from '../../app/common/form/MyTextInput';
 import { Button } from 'semantic-ui-react';
+import { useDispatch } from 'react-redux';
+import { signInUser } from './authActions';
+import { closeModal } from '../../app/common/modals/modalsReducer';
 
 export default function LoginForm() {
+	const dispatch = useDispatch();
+
 	return (
 		<ModalWrapper size='mini' header='Sign in to Re-vents'>
 			<Formik
@@ -17,7 +22,11 @@ export default function LoginForm() {
 					email: Yup.string().required().email(),
 					password: Yup.string().required(),
 				})}
-				onSubmit={(values) => console.log(values)}
+				onSubmit={(values, { setSubmitting }) => {
+					dispatch(signInUser(values));
+					setSubmitting(false);
+					dispatch(closeModal());
+				}}
 			>
 				{({ isSubmitting, isValid, dirty }) => (
 					<Form className='ui form'>
